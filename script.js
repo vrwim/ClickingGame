58efc183f8efe458c689385586b86df55ab359c5
Wim Van Renterghem <git@vrwim.com>
2018-02-14 12:28:08 +0100

    Updated to include data from all participants

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Begin het spel met 0 totale clicks
+// Begin het spel met 0 clicks
 var clicks = 0;
+
+// Begin het spel met 0 totale clicks
+var totalClicks = 0;
 
 // Begin het spel met 0 punten
 var totalPoints = 0;
+
+// Aantal gebruikers
+var users = 0;
 
 // Hoeveel kliks er nodig zijn voor het volgende punt
 function clicksToNextPoint() {
@@ -20,10 +26,11 @@ function click() {
 	// Binnen deze accolades wordt uitgevoerd bij het klikken op de knop
 
 	// Voeg 1 toe aan de totalPoints
-	clicks = clicks + 1;
+	clicks++;
+	totalClicks++;
 
 	// We kunnen ook dingen in de log schrijven, zodat we kunnen zien wat er gebeurt
-	console.log("Clicks nodig om naar het volgende level te gaan: ", clicksToNextPoint());
+	// console.log("Clicks nodig om naar het volgende level te gaan: ", clicksToNextPoint());
 
 	// Als clicks om het volgende level te bereiken gelijk is aan 0, dan hebben we het volgende level bereikt
 	if(clicksToNextPoint() == clicks) {
@@ -39,16 +46,40 @@ function click() {
 
 	// Willekeurig getal tussen 0 en 1, als het groter is dan 1/2, toon de ene afbeelding, andere afbeelding verbergen
 	if(Math.random() >= 0.5) {
-		console.log("Show right image");
 		document.getElementById('imgLeft').style.visibility = 'hidden';
 		document.getElementById('imgRight').style.visibility = 'visible';
 	} else {
-		console.log("Show left image");
 		document.getElementById('imgLeft').style.visibility = 'visible';
 		document.getElementById('imgRight').style.visibility = 'hidden';
 	}
 }
 
+function clickStop() {
+
+	users += 1;
+
+	var table = document.getElementById("resultsTable");
+	var row = table.insertRow(table.childNodes.length - 1);
+
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+
+	cell1.innerHTML = "Gebruiker " + users;
+	cell2.innerHTML = totalPoints;
+	cell3.innerHTML = clicks;
+	cell4.innerHTML = totalClicks;
+
+	// Reset alles
+	clicks = 0;
+	totalClicks = 0;
+	totalPoints = 0;
+}
+
+document.getElementById('resultsTable').style.visibility = 'hidden';
+
 // Negeer dit :)
 document.getElementById("imgLeft").onclick = click;
 document.getElementById("imgRight").onclick = click;
+document.getElementById("stopButton").onclick = clickStop;
